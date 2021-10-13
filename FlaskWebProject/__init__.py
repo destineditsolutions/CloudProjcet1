4919d760b8a1184f1c5c2f1810c27d3819af1895
Emmanuel A <39139165+destineditsolutions@users.noreply.github.com>
2021-10-13 12:04:54 -0500

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,7 +11,9 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 wsgi_app = app.wsgi_app
+
 app.logger.setLevel(logging.INFO)
+
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
