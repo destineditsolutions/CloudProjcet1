f3e5f9b010e3589973f0655e3cbc83034110c0db
Emmanuel A <39139165+destineditsolutions@users.noreply.github.com>
2021-10-19 18:09:23 -0500

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class Config(object):
 
     CLIENT_ID = "204b80ac-a556-4e94-8929-044b67c4789b"
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "https://cmspythonpros.azurewebsites.net/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
