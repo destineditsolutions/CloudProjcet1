27ad323a2751da5a06265a0c7bc331173a3e6daa
Emmanuel A <39139165+destineditsolutions@users.noreply.github.com>
2021-10-19 17:06:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class Config(object):
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "62ca8c5f-d061-40cb-a2f5-a40650d1d68a"
+    CLIENT_ID = "204b80ac-a556-4e94-8929-044b67c4789b"
 
     REDIRECT_PATH = "https://cmspythonappnw.azurewebsites.net/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
