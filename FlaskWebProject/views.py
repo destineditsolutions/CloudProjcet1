af100fe30e12e91a5ed8c97fd1b5b3db3f1f89f9
Emmanuel A <39139165+destineditsolutions@users.noreply.github.com>
2021-11-03 08:54:48 -0500

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -77,31 +77,31 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-            app.logger.info("Login Successful")
+            app.logger.info('Login Successful')
         return redirect(next_page)
-    session["state"] = str(uuid.uuid4())
-    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
+    session['state'] = str(uuid.uuid4())
+    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session['state'])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
-    if request.args.get('state') != session.get("state"):
+    if request.args.get('state') != session.get('state'):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
-    if "error" in request.args:  # Authentication/Authorization failure
-        return render_template("auth_error.html", result=request.args)
+    if 'error' in request.args:  # Authentication/Authorization failure
+        return render_template('auth_error.html', result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https'))
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
+        if 'error' in result:
+            return render_template('auth_error.html', result=result)
+        session['user'] = result.get('id_token_claims')
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
-        user = User.query.filter_by(username="admin").first()
+        user = User.query.filter_by(username='admin').first()
         login_user(user)
         _save_cache(cache)
     return redirect(url_for('home'))
@@ -110,13 +110,13 @@ def authorized():
 @app.route('/logout')
 def logout():
     logout_user()
-    if session.get("user"): # Used MS Login
+    if session.get('user'): # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
         # Also logout from your tenant's web session
         return redirect(
-            Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True))
+            Config.AUTHORITY + '/oauth2/v2.0/logout' +
+            '?post_logout_redirect_uri=' + url_for('login', _external=True))
 
     return redirect(url_for('login'))
 
